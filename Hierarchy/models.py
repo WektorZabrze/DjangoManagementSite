0cf6a8122d7980634508e600f65aba57f995a3d3
Loniowsky <jjcierpich@interia.pl>
2018-04-11 15:32:21 +0200

    person task field

--- Hierarchy/models.py
+++ Hierarchy/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import validate_comma_separated_integer_list
 
 class Person(models.Model):
 	first_name= models.CharField(max_length = 100)
@@ -6,7 +7,7 @@ class Person(models.Model):
 	age = models.IntegerField()
 	personal_id = models.AutoField(primary_key = True)
 	position = models.CharField(max_length = 200)
-	#tasks_ids = models.ListIntegerField()
+	tasks_ids = models.CharField(validators=[validate_comma_separated_integer_list],max_length=200, default="")
 	#subordinates_id = models.ListIntegerField()
 
 	def __str__(self):
