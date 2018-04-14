10378f40a6da8f1fe2ed5bcd030583a3e9778181
Pawlllosss <oczadlypawe@gmail.com>
2018-04-14 23:54:50 +0200

    Created a simple, not fully functional search form

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,22 @@
 from .models import Task
+from .filters import TaskFilter
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 
 
 
-#it was previously  name display_tasks - I think current name is better because we can see with wich template it's related - Faplo
+#Faplo - currently working 14.04
 def tasks_list(request):
     tasks_table = Task.objects.all();
     return render(request, 'tasks/tasks_list.html', locals())
 
+#Faplo - currently working 14.04
+def search_task(request):
+    tasks_table = Task.objects.all()
+    tasks_filter = TaskFilter(request.GET, queryset=tasks_table)
+    return render(request, 'tasks/task_search.html', {'filter': tasks_filter})
 
+#Marcin
 class TaskCreate(CreateView):
     model = Task
     fields = ['assigned_employee', 'task_name', 'task_description', 'priority', 'created_date', 'deadline_date']
