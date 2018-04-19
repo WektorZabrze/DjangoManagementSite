1055c4ce8d5d7fa3b2cb566fd880ed52bc1bc60b
mrcmis <marcinxmis@gmail.com>
2018-04-19 22:36:48 +0200

    changed adding tasks, basically it works the same but there was created forms.py file for storing forms.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
-from . import views
 
-from .filters import TaskFilter
-from django_filters.views import FilterView
+from . import views
 
 urlpatterns = [
     url(r'^$', views.tasks_list, name='tasks_list'),
-    url(r'add/$', views.TaskCreate.as_view(), name='task-add'),
+    url(r'add/$', views.task_add, name='task-add'),
     url(r'search/$', views.search_task, name='task-search'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,22 +1,32 @@
-from .models import Task
-from .filters import TaskFilter
+from django.shortcuts import redirect
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+
+from .filters import TaskFilter
+from .forms import TaskForm
+from .models import Task
 
 
-
-#Faplo - currently working 14.04
+# Faplo - currently working 14.04
 def tasks_list(request):
     tasks_table = Task.objects.all();
     return render(request, 'tasks/tasks_list.html', locals())
 
-#Faplo - currently working 14.04
+
+# Faplo - currently working 14.04
 def search_task(request):
     tasks_table = Task.objects.all()
     tasks_filter = TaskFilter(request.GET, queryset=tasks_table)
     return render(request, 'tasks/task_search.html', {'filter': tasks_filter})
 
-#Marcin
-class TaskCreate(CreateView):
-    model = Task
-    fields = ['assigned_employee', 'task_name', 'task_description', 'priority', 'created_date', 'deadline_date']
+
+# Marcin changed 19.04 and working
+def task_add(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+            return redirect('tasks_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
