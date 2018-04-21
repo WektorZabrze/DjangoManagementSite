2456461a7712024d017030ad2d2c4c21ce987b7c
arokasprz100 <arkadiusz.kasprzak@onet.pl>
2018-04-21 12:44:46 +0200

    templates

--- users/views.py
+++ users/views.py
@@ -11,39 +11,8 @@ def display_users(request):
 	return HttpResponse(html)
 
 def index(request):
-	if not request.user.is_authenticated:
-		return default_view()
-	elif request.user.position == 'BOS':
-		return boss_view()
-	elif request.user.position == 'MAN':
-		return manager_view()
-	elif request.user.position == 'SUP':
-		return supervisor_view()
-	elif request.user.position == 'WOR':
-		return worker_view()	
-	elif request.user.position:
-		return whatever()
-	else:
-		return default_view()
+	return render(request, 'user_views/uniformed_view.html')
 
-def boss_view():
-	return HttpResponse(loader.get_template("user_views/boss_view.html").render())
-
-def manager_view():
-	return HttpResponse(loader.get_template("user_views/manager_view.html").render())
-
-def supervisor_view():
-	return HttpResponse(loader.get_template("user_views/supervisor_view.html").render())
-
-def worker_view():
-	return HttpResponse(loader.get_template("user_views/worker_view.html").render())
-
-def default_view():
-	return HttpResponse(loader.get_template("user_views/default_view.html").render())
-	
-
-def whatever():
-	return HttpResponse("<html><body> You are logged in!</html><body>")
 
 def logout_user(request):
 	return logout(request)
