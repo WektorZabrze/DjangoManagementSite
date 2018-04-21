03e464e12ec82032820f9a3f3c38e6f87cabebc3
Loniowsky <jjcierpich@interia.pl>
2018-04-21 12:45:44 +0200

    2nd part of merging

--- tasks/models.py
+++ tasks/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 from users.models import Person
 from django.utils import timezone
-from model_utils import Choices
 
 #we're gonna stick to it for now - Faplo 13.04.2018
 class Task(models.Model):
     #(database value, code-readable value, human-readable value)
     #maybe modify values names so the code-readable and human-readable values won't be mistaken - Faplo
-    PRIORITY_VALUES = Choices(
-        (1, 'low', "Low"),
-        (2, 'medium', 'Medium'),
-        (3, 'high', 'High'),
-        (4, 'critical', 'Critical'),
-    )
+    priority = models.CharField(max_length = 3, choices =(
+		('LOW', 'Low'),
+		('MED', 'Medium'),
+		('HIG', 'High'),
+		('CRI', 'Critical'),
+		), default = 'WOR',
+	)
 
     assigned_employee = models.ForeignKey(Person, on_delete=models.CASCADE)
     task_name = models.CharField(max_length=100)
     task_description = models.TextField()
-    priority = models.IntegerField(default=PRIORITY_VALUES.medium, choices = PRIORITY_VALUES)
     created_date = models.DateTimeField(
             default=timezone.now)
     deadline_date = models.DateTimeField(
