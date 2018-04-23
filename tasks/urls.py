76d324f5e7a64b6fa17fdda5e0c881e8c6419644
mrcmis <marcinxmis@gmail.com>
2018-04-23 21:30:58 +0200

    Added first version of representation 2D based on js chart

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'add/$', views.task_add, name='task-add'),
     url(r'search/$', views.search_task, name='task-search'),
     url(r'edit/(?P<pk>\d+)/$', views.task_edit, name='task-edit'),
+    url(r'api/chart/data/$', views.ChartData.as_view(), name='chart-data'),
+    url(r'chart/$', views.get_chart, name='chart-view'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
 from .filters import TaskFilter
 from .forms import TaskForm
@@ -45,3 +47,38 @@ def task_edit(request, pk):
     else:
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_edit.html', {'form': form})
+
+
+def get_chart(request):
+    return render(request, 'tasks/chart/chart.html')
+
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request):
+        # example, temporary values
+        x_values = [1, 2, 3, 4, 5, 6, 7, 8]
+        y_values = [1, 2, 3, 4, 5, 6, 7, 8]
+        tasks_descriptions = ["task desc 1", "task desc 2", "task desc 3", "task desc 4", "task desc 5", "task desc 6",
+                              "task desc 7", "task desc 8"]
+
+        #Here we need to build proper JSON structure as Chart.js we use require such
+        all_tasks_chart_data = []
+        for i in range(len(x_values)):
+            chart_point_settings = []
+            chart_point_settings.append({"x": x_values[i],
+                                         "y": y_values[i],
+                                         "r": 15})
+            single_task_data = {
+                "label": tasks_descriptions[i],
+                "data": chart_point_settings,
+                "backgroundColor": "rgba(255,221,50,0.2)",
+                "borderColor": "rgba(255,221,50,1)",
+            }
+            all_tasks_chart_data.append(single_task_data)
+
+        return JsonResponse({
+            "tasks_data": all_tasks_chart_data
+        })
