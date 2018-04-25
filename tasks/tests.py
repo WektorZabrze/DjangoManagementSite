d5e17442167e771f218e94b245ec9f0d8914bf5c
arokasprz100 <arkadiusz.kasprzak@onet.pl>
2018-04-25 10:48:19 +0200

    some tasks tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+class TaskTestCase(TestCase):
+
+	def test_get_chart(self):
+		url = '/tasks/chart/'
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+		self.assertTemplateUsed(response, 'tasks/chart/chart.html')
+
+	def test_user_tasks(self):
+		url = '/tasks'
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+		# Test situation when user is not logged in
+		self.assertTemplateUsed(response, 'user_views/uniformed_view.html')
