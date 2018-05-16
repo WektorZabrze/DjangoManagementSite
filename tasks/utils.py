0d91ad3affccc3f73b1f384fae40a18cdf6636f6
mrcmis <marcinxmis@gmail.com>
2018-05-16 21:28:36 +0200

    first version of calculating task performance index algorithm

--- tasks/utils.py
+++ tasks/utils.py
@@ -1,4 +1,15 @@
 def calculate_performance_index(task):
-    numerator = 10
-    denominator = 2
-    return numerator / denominator
+    if task.priority == 'LOW':
+        ratio = 0.6
+    elif task.priority == 'HIG':
+        ratio = 1.4
+    elif task.priority == 'CRI':
+        ratio = 1.8
+    else:
+        ratio = 1.0
+
+    numerator = (task.deadline_date.date() - task.end_date.date()).total_seconds()*ratio
+    denominator = (task.deadline_date.date() - task.created_date.date()).total_seconds()
+
+    return float("{0:.2f}".format(numerator/denominator))
+
