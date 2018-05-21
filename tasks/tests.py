668ce43d69b629c6f3bc6330a48834a8a93a0d41
arokasprz100 <arkadiusz.kasprzak@onet.pl>
2018-05-21 11:43:41 +0200

    more tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,10 +1,12 @@
 from django.test import TestCase
 from .models import Task
 from gensim.models import Doc2Vec
+from users.models import Person
+from .apps import TasksConfig
 
 from .text_dimensionality_reduction import textdimensionalityreduction as tdr
 
-class TaskTestCase(TestCase):
+class TaskViewsTestCase(TestCase):
 
 	def test_get_chart(self):
 		url = '/tasks/chart/'
@@ -20,7 +22,7 @@ class TaskTestCase(TestCase):
 		self.assertTemplateUsed(response, 'user_views/uniformed_view.html')
 
 
-class TextReductionTestCaset(TestCase):
+class TextReductionTestCase(TestCase):
 
 	def test_createSentenceVectors(self):
 		gatheredWords = []
@@ -28,11 +30,32 @@ class TextReductionTestCaset(TestCase):
 		model = Doc2Vec.load("tasks/text_dimensionality_reduction/doc2vecmodel")
 		self.assertIsInstance(tdr.createSentenceVectors(gatheredWords, model), list)
 
-# class TaskModelTestCase(TestCase):
-# 	@classmethod
-# 	def setUpTestData(cls):
-# 		task1 = Task.objects.create() 
+class TaskModelTestCase(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		cls.person = Person.objects.create_user(username = 'temp', password = 'temp', position = 'BOS')
+		cls.task1 = Task.objects.create(priority = 'LOW',assigned_employee = cls.person,  task_name = 'dummy task', 
+			task_description = 'dummy_task', created_date = '1900-01-01', 
+			deadline_date = '2030-01-01') 
 
-# 	def test_get_absolute_url(self):
-# 		self.assertIsInstance(Task.get_absolute_url(), str)
-# 		self.assertEquals(Task.get_absolute_url(), '/tasks/')
+	def test_get_absolute_url(self):
+		self.assertIsInstance(self.task1.get_absolute_url(), str)
+		self.assertEquals(self.task1.get_absolute_url(), '/tasks/')
+
+	def test__str__(self):
+		self.assertEquals(str(self.task1), self.task1.task_name)
+
+	def test_fields(self):
+		self.assertEquals(self.task1.priority, 'LOW')
+		self.assertEquals(self.task1.assigned_employee, self.person)
+		self.assertEquals(self.task1.task_name, 'dummy task')
+		self.assertEquals(self.task1.task_description, 'dummy_task')
+		self.assertEquals(self.task1.created_date, '1900-01-01')
+		self.assertEquals(self.task1.deadline_date, '2030-01-01')
+
+
+class TaskAppsTestCase(TestCase):
+	'''Test apps module'''
+	def test_TasksConfig(self):
+		''' Test task name'''
+		self.assertEquals(TasksConfig.name, 'tasks')
