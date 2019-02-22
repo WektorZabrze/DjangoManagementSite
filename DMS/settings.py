42be0993868d257b356385e7c0bb39104a498994
Loniowsky <jjcierpich@gmail.com>
2019-02-22 11:43:10 +0100

    bck

--- DMS/settings.py
+++ DMS/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'django_tables2',
     'users',
     'tasks',
